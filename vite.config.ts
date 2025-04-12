4991bc5b81775a6e094fd1284ed4ecfccec9e7f5
Andrey Turov <andrey.turoff@gmail.com>
2025-04-12 22:11:59 +0300

    wtf fix3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import vitePluginRequire from "vite-plugin-require"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vitePluginRequire from "vite-plugin-require";
 
 // https://vite.dev/config/
 export default defineConfig({
