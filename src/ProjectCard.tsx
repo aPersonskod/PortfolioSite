8683c84544a08d79b543b66859f7585c115e3657
Andrey Turov <andrey.turoff@gmail.com>
2025-04-12 15:31:53 +0300

    dev cards hovering

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
 function ProjectCard({imgSrc, imgAlt, header, description}) {
+    const [isHovering, setIsHovering] = useState(false);
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    };
+
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    };
+    
+    const imgStyle = {
+        maxWidth: '492px',
+        width: '100%',
+        height: 'auto',
+        borderRadius: '21px',
+        transition: '0.3s ease-in-out'  
+    };
+    const imgHoverStyle = {
+        maxWidth: '492px',
+        width: '103%',
+        height: 'auto',
+        borderRadius: '21px',
+        transition: '0.3s ease-in-out'
+    };
+    const pStyle = {
+        backgroundColor: 'transparent',
+    }
+    const pHoverStyle = {
+        backgroundColor: '#979797'
+    }
+    
     return (
-        <div>
-            <img className='responsive' src={imgSrc} alt={imgAlt}/>
-            <p className='fs24'>{header}</p>
+        <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            <img style={isHovering ? imgHoverStyle : imgStyle} src={imgSrc} alt={imgAlt}/>
+            <p className='fs24' style={isHovering ? pHoverStyle : pStyle}>{header}</p>
             <p className='fs18 color-gray' style={{marginTop:"-20px"}}>{description}</p>
         </div>
     );
