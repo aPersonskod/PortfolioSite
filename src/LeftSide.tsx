9f314c6e1ca79a48f3b8b305e9c118d5cf7f33fa
Andrey Turov <andrey.turoff@gmail.com>
2025-04-13 03:19:43 +0300

    dev back button

--- src/LeftSide.tsx
+++ src/LeftSide.tsx
@@ -4,11 +4,22 @@ import './App.css'
 
 function LeftSide() {
     const [active, setActive] = useState("");
+    const [visibility, setVisibility] = useState(true);
+    const visStyle = {display:'block'};
+    const unvisStyle = {display:'none'};
     const navigate = useNavigate();
     
     useEffect(() => {
         handleClick();
-    }, [])
+        switch (window.location.pathname){
+            case '/PortfolioSite/project/uploader': setVisibility(false); break;
+            case '/PortfolioSite/project/site': setVisibility(false); break;
+            case '/PortfolioSite/project/schedule': setVisibility(false); break;
+            case '/PortfolioSite/project/avto': setVisibility(false); break;
+            case '/PortfolioSite/project/puti': setVisibility(false); break;
+            default: setVisibility(true); break;
+        }
+    }, [window.location.pathname]);
     
     const handleClick = () => {
         setActive(window.location.pathname);
@@ -38,8 +49,15 @@ function LeftSide() {
                 <div className='myBtns'>
                     <div style={{display: 'flex', justifyContent: 'space-between', width: '350px'}}>
                         <button className={active === "/PortfolioSite" ? "active" : undefined}
-                                onClick={projectsNavigation}>
+                                onClick={projectsNavigation} style={visibility ? visStyle : unvisStyle}>
                             Проекты
+                        </button>
+                        <button className={"active"}
+                                onClick={projectsNavigation} style={!visibility ? visStyle : unvisStyle}>
+                            <div className='d-flex justify-content-around' style={{maxHeight:'15px', marginTop:'-10px'}}>
+                                <img style={{marginTop:'8px', marginRight:'3px'}} src={require('./assets/backIcon.svg')} alt="back" width='10px' height='10px' />    
+                                <p>Назад</p>
+                            </div>
                         </button>
                         <button className={active === "/PortfolioSite/about" ? "active" : undefined}
                                 onClick={aboutNavigation}>
